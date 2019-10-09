c5e7caeb8ced578aadf2145acb4ca30d097b6f43
João Victor <joaodjtr@gmail.com>
2019-10-09 14:36:13 -0300

    Finishing profile visitation
    Now, it's possible to visit NGOs profiles logged as volunteer or NGO.

--- Routers/home.js
+++ Routers/home.js
@@ -5,29 +5,22 @@ const actionController = require("../controllers/actionController")
 router.get("/", async (req, res) => {
     if(req.session.ngo){
         let activeActions = await actionController.listActiveActionsNgo(req.session.ngo.idNgo)
-        
         for(let action of activeActions) action = formatAction(action)
-        
         let actions = {
             activeActions
         }
-        
         if(actions.activeActions.length == 0) actions = false
-
         res.render("ngo/Home", {dataHeaderNgo: req.session.ngo, actions})
+
     }else{
         let recommendedActions = await actionController.listRecommendedActions(req.session.user.idVolunteer)
-        
         // Format each action
         for(let action of recommendedActions) action = formatAction(action)
-        
         let actions = {
             recommendedActions
         }
-
-        // Set to null events if is empty
+        // Set to null actions if is empty
         if(actions.recommendedActions.length == 0) actions = false
-        
         res.render("user/home", {dataHeader: req.session.user, ngos: req.session.ngoUser, actions})
     }
 })
--- Routers/user.js
+++ Routers/user.js
@@ -7,40 +7,48 @@ const verifyUserName = require("../helpers/verifyUserName")
 router.get('/:userName', async (req,res)=>{
     const months = ["Jan", "Fev", "Mar", "Abri", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
 
+    // Render volunteer own profile
+    if(req.params.userName === req.session.user.userName){
+        req.session.user.createdAt = new Date(req.session.user.createdAt)
+        req.session.user.createdAt.month = months[req.session.user.createdAt.getMonth()]
+        req.session.user.createdAt.year = req.session.user.createdAt.getFullYear()
+        const category = await causesController.listCausesUser(req.session.user.idVolunteer)
+        return res.render("user/profile", {data: req.session.user, dataHeader: req.session.user, causes: category, ngos: req.session.ngoUser, isVisit: false, isVolunteer: true})
+    }
+
+    // Render other volunteer profile
+    const user = await verifyUserName.user(req.params.userName)
+    if(user){
+        user.createdAt.month = months[user.createdAt.getMonth()]
+        user.createdAt.year = user.createdAt.getFullYear()
+        const category = await causesController.listCausesUser(user.idVolunteer)
+        return res.render("user/profile", {data: user, dataHeader: req.session.user, causes: category, ngos: req.session.ngoUser, isVisit: true, isVolunteer: true})
+    }
+
+    // Render ngo own profile
     if(req.session.ngo){
         if(req.params.userName === req.session.ngo.userName){
             req.session.ngo.createdAt = new Date(req.session.ngo.createdAt)
             req.session.ngo.createdAt.month = months[req.session.ngo.createdAt.getMonth()]
             req.session.ngo.createdAt.year = req.session.ngo.createdAt.getFullYear()
             const category = await causesController.listCausesNgo(req.session.ngo.idNgo)
-            return res.render("ngo/profile", {data: req.session.ngo, dataHeaderNgo: req.session.ngo, causes: category})
+            return res.render("ngo/profile", {data: req.session.ngo, dataHeaderNgo: req.session.ngo, causes: category, isVisit: false})
         }
     }
 
-    if(req.params.userName === req.session.user.userName){
-        req.session.user.createdAt = new Date(req.session.user.createdAt)
-        req.session.user.createdAt.month = months[req.session.user.createdAt.getMonth()]
-        req.session.user.createdAt.year = req.session.user.createdAt.getFullYear()
-        const category = await causesController.listCausesUser(req.session.user.idVolunteer)
-        return res.render("user/profile", {data: req.session.user, causes: category, dataHeader: req.session.user})
-    }
-
-    const user = await verifyUserName.user(req.params.userName)
-    if(user){
-        user.createdAt.month = months[user.createdAt.getMonth()]
-        user.createdAt.year = user.createdAt.getFullYear()
-        const category = await causesController.listCausesUser(user.idVolunteer)
-        return res.render("user/profile", {data: user, causes: category, dataHeader: req.session.user})
-    }
-
+    // Render other ngo profile
     const ngo = await verifyUserName.ngo(req.params.userName)
     if(ngo){
         ngo.createdAt.month = months[ngo.createdAt.getMonth()]
         ngo.createdAt.year = ngo.createdAt.getFullYear()
         const category = await causesController.listCausesNgo(ngo.idNgo)
-        return res.render("ngo/profile", {data: ngo, causes: category, dataHeader: req.session.user})
+
+        // Checks if user logged is a volunteer or not
+        /* is volunteer */ if(!req.session.ngo) return res.render("ngo/profile", {data: ngo, dataHeader: req.session.user, causes: category, ngos: req.session.ngoUser, isVisit: true, isVolunteer: true})
+        /* isnt volunteer */ else return res.render("ngo/profile", {data: ngo, dataHeaderNgo: req.session.ngo, causes: category, isVisit: true, isVolunteer: false})
     }
     
+    // If not found any volunteer or ngo
     if(req.session.ngo){
         return res.render('error', {dataHeaderNgo: req.session.ngo})
     }else{
