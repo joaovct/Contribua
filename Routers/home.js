bf02d2c0d9e812020326ce59a5ea050e855b5e5d
João Victor <joaodjtr@gmail.com>
2019-10-21 21:12:31 -0300

    Feed filters - front-end

--- Routers/home.js
+++ Routers/home.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const actionController = require("../controllers/actionController")
+const causesController = require('../controllers/causesController')
 const userNgoController = require("../controllers/userNgoController")
 const feedUtilities = require('../helpers/feedUtilities')
 
 router.get("/", async (req, res) => {
+    const causes = await causesController.listCauses()
     if(req.session.ngo){
         let activeActions = await actionController.listActiveActionsNgo(req.session.ngo.idNgo)
         for(let action of activeActions) action = feedUtilities.formatAction(action)
@@ -12,11 +14,13 @@ router.get("/", async (req, res) => {
             activeActions
         }
         if(actions.activeActions.length == 0) actions = false
-        res.render("ngo/Home", {dataHeaderNgo: req.session.ngo, actions})
+        res.render("ngo/Home", {dataHeaderNgo: req.session.ngo, actions, causes})
 
     }else{
         let recommendedActions = await actionController.listRecommendedActions(req.session.user.idVolunteer)
         let recommendedNgos = await userNgoController.listRecommendedNgos(req.session.user.idVolunteer)
+        const userCauses = await causesController.listCausesUser(req.session.user.idVolunteer)
+        const causesNotParticipe = await causesController.listCausesNotParticipeUser(req.session.user.idVolunteer)
         // Format each action
         for(let action of recommendedActions) action = feedUtilities.formatAction(action)
         let actions = {
@@ -24,7 +28,7 @@ router.get("/", async (req, res) => {
         }
         // Set to null actions if is empty
         if(actions.recommendedActions.length == 0) actions = false
-        res.render("user/home", {dataHeader: req.session.user, ngos: req.session.ngoUser, actions, recommendedNgos})
+        res.render("user/home", {dataHeader: req.session.user, ngos: req.session.ngoUser, actions, recommendedNgos, userCauses, causesNotParticipe})
     }
 })
 
